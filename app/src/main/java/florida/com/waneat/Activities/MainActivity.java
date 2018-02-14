5845b524979ccb2849e51d0311cda9690a71ac2d
JorgeYYA <ies25a.jorgeq@gmail.com>
2018-02-14 19:49:26 +0100

    recuperar pedidos v2

--- app/src/main/java/florida/com/waneat/Activities/MainActivity.java
+++ app/src/main/java/florida/com/waneat/Activities/MainActivity.java
@@ -1,6 +1,9 @@
 package florida.com.waneat.Activities;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -273,6 +276,22 @@ public class MainActivity extends AppCompatActivity implements
             }
             added = false;
         }
+    }
+
+    public static boolean verificaConexion(Context ctx) {
+        boolean bConectado = false;
+        ConnectivityManager connec = (ConnectivityManager) ctx
+                .getSystemService(Context.CONNECTIVITY_SERVICE);
+        // No sólo wifi, también GPRS
+        NetworkInfo[] redes = connec.getAllNetworkInfo();
+        // este bucle debería no ser tan ñapa
+        for (int i = 0; i < 2; i++) {
+            // ¿Tenemos conexión? ponemos a true
+            if (redes[i].getState() == NetworkInfo.State.CONNECTED) {
+                bConectado = true;
+            }
+        }
+        return bConectado;
     }
 
     private void cargarProductosIniciales(){
