5d27f009ab5ff0650465fba0ac9294dcc10bbef6
David5002WL <david_lolwow1@hotmail.com>
2018-02-16 19:32:17 +0100

    poca cosa, la verdad...

--- app/src/main/java/florida/com/waneat/Fragments/DialogProductRating.java
+++ app/src/main/java/florida/com/waneat/Fragments/DialogProductRating.java
@@ -1,9 +1,11 @@
 package florida.com.waneat.Fragments;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RatingBar;
 import android.widget.TextView;
 
 import florida.com.waneat.R;
@@ -12,6 +14,7 @@ import florida.com.waneat.R;
 public class DialogProductRating extends android.support.v4.app.DialogFragment {
 
     private TextView textCancelar1, textAceptar1;
+    private RatingBar ratingBar1;
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -23,6 +26,7 @@ public class DialogProductRating extends android.support.v4.app.DialogFragment {
 
         textCancelar1 = (TextView) rootView.findViewById(R.id.textCancelar1);
         textAceptar1 = (TextView) rootView.findViewById(R.id.textAceptar1);
+        ratingBar1 = (RatingBar) rootView.findViewById(R.id.ratingBar1);
 
         textCancelar1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -34,7 +38,8 @@ public class DialogProductRating extends android.support.v4.app.DialogFragment {
         textAceptar1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                getDialog().dismiss();
+                Log.d("estrellas", "puntuacion: "+ratingBar1.getRating());
             }
         });
 
